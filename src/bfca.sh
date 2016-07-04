9156026a44d281cec7063db1b2ba15456fc56e42
David H. Christensen <me@davidh.info>
2016-07-04 17:58:44 +0000

    Improved bfca frontend

--- src/bfca.sh
+++ src/bfca.sh
@@ -3,8 +3,11 @@ OUTPUTFILE=$2
 if [[ -z $2 ]]; then
 	OUTPUTFILE="bf.out"
 fi
-echo "Compiling to $OUTPUTFILE..."
+echo "Compiling $1"
 cat $1 | bfca.codegen > __BFK_TEMP.s
+
+echo "Assembling and linking $OUTPUTFILE"
 as __BFK_TEMP.s -o __BFK_TEMP.o
-cc __BFK_TEMP.o -o $OUTPUTFILE
+cc -s __BFK_TEMP.o -o $OUTPUTFILE
 rm -f __BFK_TEMP.*
+echo "Done."
